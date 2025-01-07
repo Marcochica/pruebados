d6d6a9bb924b81b198a97a6f8a6bb6e3ef1fdfcd
markusvitru <markusvitru@gmail.com>
2025-01-07 14:45:44 +0100

    Se termina maquetacion, falta responsive

--- footer.php
+++ footer.php
@@ -4,7 +4,21 @@
 */
 ?>
     <footer>
-      <p>Copyright &copy; <?php echo date("Y"); ?></p>
+      <div class="footer-top">
+        <div class="footer-logo">
+          <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="Logo" width="100px" height="100px">
+          </a>
+        </div>
+        <div class="footer-address">
+          <span>Contacto</span>
+          <p>contacto@empresa.com</p>
+          <p>672 843221</p>
+        </div>
+      </div>
+      <div class="section-orange">
+        <span>Copyright &copy; <?php echo date("Y"); ?></span>
+      <div>
     </footer>
     <?php wp_footer(); ?>
   </body>
--- template-parts/home.php
+++ template-parts/home.php
@@ -19,4 +19,40 @@
         />
     </div>
     <div id="map"></div>
+    <div class="banner-center">
+        <div class="content-text-banner-center">
+            <p class="text-banner-center">
+                <span class="background-text">GASOLINERAS <br><span class="digit">+350</span><br>Gasolineras activas en España</span>
+            </p>
+            <p class="text-banner-center">
+                <span class="background-text">GASOLINERAS <br><span class="digit">+350</span><br>Gasolineras activas en España</span>
+            </p>
+            <p class="text-banner-center">
+                <span class="background-text">GASOLINERAS <br><span class="digit">+350</span><br>Gasolineras activas en España</span>
+            </p>
+        </div>
+        <img src="<?php echo get_template_directory_uri(); ?>/images/banner-center.jpg" class="img-center" alt="Banner Center">
+    </div>
+    <div class="section-info">
+        <div class="section-right rectangles">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/gas-station.jpg" class="" alt="Estación de combustible">
+        </div>
+        <div class="section-text-rectangles rectangles">
+            <span class="title-rectangles">Lorem Ipsum is printing</span>
+            <p class="text-images text-right">
+                Lorem Ipsum is printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+        </div>
+        <div class="section-bottom section-text-rectangles rectangles">
+        <span class="title-rectangles">Lorem Ipsum is printing</span>
+            <p class="text-images text-right">
+                Lorem Ipsum is printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+        </div>
+        <div class="section-bottom rectangles">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/fuel.jpg" class="" alt="Combustible">
+        </div>
+    </div>
 </div>
