9cec4f42bf3f090159c215b9b56d29563fcb29b5
markusvitru <markusvitru@gmail.com>
2025-01-07 12:22:46 +0100

    Se agrega baner principal, se sección con texto y buscador arriba del map y se cambia logo

--- template-parts/home.php
+++ template-parts/home.php
@@ -5,12 +5,18 @@
 ?>
 
 <div>
-    
-    <input
-      id="pac-input"
-      class="controls"
-      type="text"
-      placeholder="Search Box"
-    />
+    <div class="banner-home">
+        <p class="text-banner">Lorem Ipsum is printing and typesetting industry.</p>
+        <img src="<?php echo get_template_directory_uri(); ?>/images/banner-home.jpg" alt="Banner Home">
+    </div>
+    <div class="text-search">
+        <p>Utiliza el buscador para encontrar la gasolinera más cercana a tu ubicación</p>
+        <input
+        id="pac-input"
+        class="controls"
+        type="text"
+        placeholder="Buscar Dirección"
+        />
+    </div>
     <div id="map"></div>
 </div>
